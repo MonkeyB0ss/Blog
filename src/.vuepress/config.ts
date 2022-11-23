ac7dfe3fc0e9d46a3d1bf56b7960f91811eee9d9
MonkeyBoss <1912290362@qq.com>
2022-11-23 22:05:22 +0800

    test
    Signed-off-by: MonkeyBoss <1912290362@qq.com>

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,76 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
+
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Blog Demo",
+      description: "A blog demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
+    },
+  },
+
+  theme,
+
+  plugins: [
+    docsearchPlugin({
+      appId: "F3AV1N1OZ3",
+      apiKey: "5e1aa71ecf13d2d101c4e2e5e870f2b4",
+      indexName: "monkeyboss",
+      locales: {
+        "/": {
+          placeholder: "搜索",
+          translations: {
+            button: {
+              buttonText: "搜索",
+              buttonAriaLabel: "搜索",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
+    }),
+  ],
+
+  shouldPrefetch: false,
+});
